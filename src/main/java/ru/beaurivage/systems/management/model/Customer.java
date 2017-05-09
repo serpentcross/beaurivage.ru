aaef52e55778c941691f37600c3a04945fefd81e
serpentcross <serpentcross@mail.ru>
2017-05-09 19:15:31 +0300

    Changed. Structure of main models.

--- src/main/java/ru/beaurivage/systems/management/model/Customer.java
+++ src/main/java/ru/beaurivage/systems/management/model/Customer.java
@@ -1,5 +1,8 @@
 package ru.beaurivage.systems.management.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,13 +12,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "customer")
-class Customer {
+class Customer implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
+    @JsonIgnoreProperties("id")
     @Getter @Setter
     private Long id;
 
@@ -39,4 +45,29 @@ class Customer {
     @Getter @Setter
     private String email;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (!id.equals(customer.id)) return false;
+        if (!firstName.equals(customer.firstName)) return false;
+        if (middleName != null ? !middleName.equals(customer.middleName) : customer.middleName != null) return false;
+        if (lastName != null ? !lastName.equals(customer.lastName) : customer.lastName != null) return false;
+        if (phone != null ? !phone.equals(customer.phone) : customer.phone != null) return false;
+        return email != null ? email.equals(customer.email) : customer.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + (middleName != null ? middleName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/ru/beaurivage/systems/management/model/Record.java
+++ src/main/java/ru/beaurivage/systems/management/model/Record.java
@@ -1,31 +1,42 @@
 package ru.beaurivage.systems.management.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import java.util.Date;
-import java.util.Set;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "record")
-public class Record {
+public class Record implements Serializable {
+
+    public Record() {
+
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonIgnore
+	@JsonIgnoreProperties("id")
     @Getter @Setter
     private Long id;
 
-    @OneToMany
+    @ManyToOne(fetch= FetchType.LAZY)
+    @JoinColumn(name="customer")
     @Getter @Setter
-    Set<Customer> customer;
+    private Customer customer;
 
     @Column(name = "day")
     @Getter @Setter
@@ -39,4 +50,27 @@ public class Record {
     @Getter @Setter
     private String time_to;
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Record record = (Record) o;
+
+		if (!id.equals(record.id)) return false;
+		if (!customer.equals(record.customer)) return false;
+		if (day != null ? !day.equals(record.day) : record.day != null) return false;
+		if (time_from != null ? !time_from.equals(record.time_from) : record.time_from != null) return false;
+		return time_to != null ? time_to.equals(record.time_to) : record.time_to == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id.hashCode();
+		result = 31 * result + customer.hashCode();
+		result = 31 * result + (day != null ? day.hashCode() : 0);
+		result = 31 * result + (time_from != null ? time_from.hashCode() : 0);
+		result = 31 * result + (time_to != null ? time_to.hashCode() : 0);
+		return result;
+	}
 }

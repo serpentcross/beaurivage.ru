ea73cbdbf45624b917af200a8f0e5d250c04d2d0
Vladimir <serpentcross@bk.ru>
2017-11-05 21:43:08 +0300

    Changed. SQL-structure

--- beaurivage-ejb/src/main/resources/META-INF/beaurivage.sql
+++ beaurivage-ejb/src/main/resources/META-INF/beaurivage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 31, 2017 at 01:54 PM
+-- Generation Time: Nov 05, 2017 at 07:41 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -34,15 +34,9 @@ CREATE TABLE `patient` (
   `middle_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) NOT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL
+  `email` varchar(255) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `patient`
---
-
-INSERT INTO `patient` (`id`, `first_name`, `middle_name`, `last_name`, `phone`, `email`) VALUES
-(35, 'Владимир', 'Владимирович', 'Андреев', '+79266530938', 'serpentcross@mail.ru');
 
 -- --------------------------------------------------------
 
@@ -55,17 +49,9 @@ CREATE TABLE `record` (
   `time_from` varchar(255) DEFAULT NULL,
   `time_to` varchar(255) DEFAULT NULL,
   `recdate` date DEFAULT NULL,
-  `patient` int(11) DEFAULT NULL
+  `patient` int(11) DEFAULT NULL,
+  `cabinet` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `record`
---
-
-INSERT INTO `record` (`id`, `time_from`, `time_to`, `recdate`, `patient`) VALUES
-(1, NULL, NULL, '2017-07-31', 35),
-(2, NULL, NULL, '2017-07-31', 35),
-(3, '12:00', '13:00', '2017-07-31', 35);
 
 -- --------------------------------------------------------
 
@@ -88,6 +74,19 @@ INSERT INTO `role` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -102,7 +101,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(4, 'beauadmin', '$2a$11$cQ/vreaYEpU1hJ9RcVcvh.hmA0.7cOlF17SwAikcLRw7tMKq059AG');
+(1, 'manager', '9fb0c3272578116a0fa404f1b22ea0203d9eb3ba');
 
 -- --------------------------------------------------------
 
@@ -114,13 +113,6 @@ CREATE TABLE `user_role` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user_role`
---
-
-INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
-(4, 1);
 
 --
 -- Indexes for dumped tables
@@ -148,6 +140,13 @@ ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `service_id_uindex` (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -168,22 +167,27 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `record`
 --
 ALTER TABLE `record`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --

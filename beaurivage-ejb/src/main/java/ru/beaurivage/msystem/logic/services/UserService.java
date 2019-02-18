eff8e7dce4718b717d83ea0e0d805bfffeac49cd
Vladimir <serpentcross@mail.ru>
2019-02-19 00:00:53 +0300

    Changed. Correct access modifiers.

--- beaurivage-ejb/src/main/java/ru/beaurivage/msystem/logic/services/UserService.java
+++ beaurivage-ejb/src/main/java/ru/beaurivage/msystem/logic/services/UserService.java
@@ -4,34 +4,34 @@ import ru.beaurivage.msystem.logic.dao.UserDAO;
 import ru.beaurivage.msystem.logic.util.EjbUtil;
 
 import java.math.BigInteger;
+
 import java.security.SecureRandom;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class UserService {
-
-    private static UserDAO userDAO;
+class UserService {
 
     private static SecureRandom random = new SecureRandom();
 
-    private static Map<String, String> rememberedUsers = new HashMap<String, String>();
+    private static Map<String, String> rememberedUsers = new HashMap<>();
 
-    public static boolean isAuthenticUser(String username, String password) {
-        userDAO = EjbUtil.getLocalBean(UserDAO.class);
+    static boolean isAuthenticUser(String username, String password) {
+        UserDAO userDAO = EjbUtil.getLocalBean(UserDAO.class);
         return userDAO.getAuthState(username, password);
     }
 
-    public static String rememberUser(String username) {
+    static String rememberUser(String username) {
         String randomId = new BigInteger(130, random).toString(32);
         rememberedUsers.put(randomId, username);
         return randomId;
     }
 
-    public static String getRememberedUser(String id) {
+    static String getRememberedUser(String id) {
         return rememberedUsers.get(id);
     }
 
-    public static void removeRememberedUser(String id) {
+    static void removeRememberedUser(String id) {
         rememberedUsers.remove(id);
     }
 

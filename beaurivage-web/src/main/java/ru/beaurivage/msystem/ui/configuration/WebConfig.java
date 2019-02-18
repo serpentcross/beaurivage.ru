73a5c42b5d73ce68589e70f491e2d4844240e284
Vladimir <serpentcross@mail.ru>
2019-02-19 00:11:21 +0300

    Changed. Formatting.

--- beaurivage-web/src/main/java/ru/beaurivage/msystem/ui/configuration/WebConfig.java
+++ beaurivage-web/src/main/java/ru/beaurivage/msystem/ui/configuration/WebConfig.java
@@ -2,6 +2,7 @@ package ru.beaurivage.msystem.ui.configuration;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
+
 import ru.beaurivage.msystem.ui.VaadinUI;
 
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,5 @@ public class WebConfig {
 
     @WebServlet(urlPatterns = "/*", asyncSupported = true)
     @VaadinServletConfiguration(ui = VaadinUI.class, productionMode = false)
-    public static class VaadinUIServlet extends VaadinServlet {
-    }
+    public static class VaadinUIServlet extends VaadinServlet {}
 }
